2a880bcce55d0ec4e7af548c57f903765ad38619
artemiys930519 <artemiys930519@yandex.ru>
2018-10-24 13:01:53 +0600

    HW3 clean sout

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -18,10 +18,8 @@ public class SpringMain {
     public static void main(String[] args) {
         // java 7 Automatic resource management
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring-test/spring-app-test.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
-            System.out.println();
 
             MealRestController mealController = appCtx.getBean(MealRestController.class);
             List<MealTo> filteredMealsWithExcess =
